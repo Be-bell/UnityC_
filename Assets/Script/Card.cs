8ed435b299842c490063c50c74a0e5411cb16eb8
choiyunhwa <sjo4208@naver.com>
2024-04-15 14:50:37 +0900

    Update > Main

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int idx = 0;
+
+    public GameObject front;
+    public GameObject back;
+    public Animator anim;
+    public SpriteRenderer frontImg;
+
+    public AudioClip clip;
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+    public void Setting(int number)
+    {
+        idx = number;
+        frontImg.sprite = Resources.Load<Sprite>($"rtan{idx}");
+    }
+    public void DestoryCard()
+    {
+        Invoke("DestoryCardInvoke", 1.0f);
+    }
+
+    private void DestoryCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+    public void OpenCard()
+    {
+        audioSource.PlayOneShot(clip);
+
+        anim.SetBool("IsOpen", true);
+        front.SetActive(true);
+        back.SetActive(false);
+
+       if(GameManager.Instance.firstCard == null)
+        {
+            GameManager.Instance.firstCard = this;
+        }
+       else
+        {
+            GameManager.Instance.secondCard = this;
+            GameManager.Instance.Matched();
+        }
+        
+    }
+
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke", 1.0f);
+    }
+
+    private void CloseCardInvoke()
+    {
+        anim.SetBool("IsOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
+    }
+}
