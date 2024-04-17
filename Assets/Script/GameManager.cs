7c9c54359452a360d252995f0e75700784142e7a
kyeongchanunity4 <kyeongchan9613@gmail.com>
2024-04-17 16:07:21 +0900

    limitedTime
    limitedTime Update

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -23,6 +23,8 @@ public class GameManager : MonoBehaviour
 
     private float time = 60.0f; // 제한 시간을 60초로 설정한다. [실패할때마다 시간 감소]
     private float maxTime = 0.0f;
+    private float executeTime = 0.0f;
+    private float waitingTime = 0.03f;
 
     public int cardCount = 0;
 
@@ -61,7 +63,26 @@ public class GameManager : MonoBehaviour
         if (time <= 10.0f) //제한시간이 10초 남았을 시 [타이머 시간 경고 기능]
         {
             timeText.color = new Color32(255, 0, 0, 255); // 제한 시간을 빨간색으로 강조 [타이머 시간 경고 기능]
+            bool check = (int)time % 2 == 1;//if문을 반복하기 위한 bool값 형성 [제한 시간 경고] 
 
+            if (check == true) //bool값이 true일때 아래 if문을 실행 [제한 시간 경고]
+            {
+                executeTime += Time.deltaTime; //실행 시간에 시간을 누적 [제한 시간 경고]
+                if (executeTime >= waitingTime) //실행 시간이 지연 시간 값을 넘을 경우 아래 if문을 실행 [제한 시간 경고]
+                {
+                    timeText.fontSize += 1; // 폰트 사이즈를 +1 만큼 올린다. [제한 시간 경고]
+                    executeTime = 0; //실행 시간 값을 초기화 [제한 시간 경고]
+                }
+            }
+            else if (check == false) //bool값이 false일때 아래 if문을 실행 [제한 시간 경고]
+            {
+                executeTime += Time.deltaTime; //실행 시간에 시간을 누적 [제한 시간 경고]
+                if (executeTime >= waitingTime) //실행 시간이 지연 시간 값을 넘을 경우 아래 if문을 실행 [제한 시간 경고]
+                {
+                    timeText.fontSize -= 1; // 폰트 사이즈를 -1 만큼 내린다. [제한 시간 경고]
+                    executeTime = 0; //실행 시간 값을 초기화 [제한 시간 경고]
+                }
+            }
             if (time <= 0.0f) //제한시간이 0초가 되었을 시 [실패할때마다 시간 감소]
             {
                 time = 0.0f; //제한시간을 0초로 고정 [실패할때마다 시간 감소]
